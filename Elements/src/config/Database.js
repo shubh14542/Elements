ed58c3b4b9406a7c0fe44127bb9c88058cdd16dd
Shubham Pande <s30771747@gmail.com>
2025-05-03 20:22:27 +0530

    Delete Elements directory

--- Elements/src/config/Database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectdb = async ()=>
-{
-  await mongoose.connect("mongodb+srv://prashantthakre712:5YGaCM3mt9v0hl1I@namastenode.t5uqi.mongodb.net/DevTinder");
-}
-
-module.exports = connectdb;
-
